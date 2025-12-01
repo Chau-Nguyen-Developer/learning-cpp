5cefbb7b56bbf36a5bbb5ef08e5edd3af9aa81a2
Chau Nguyen Developer (Christina) <chau.nguyen.dev@gmail.com>
2025-12-01 01:33:37 +0000

    Practice declaring and initializing variables

--- src/Ch01/01_03/Chau_Solution_1.cpp
+++ src/Ch01/01_03/Chau_Solution_1.cpp
@@ -1,14 +1,35 @@
 #include <iostream>
 #include <string>
 
+//Global variables. They are accessible to all parts of the code.
+//Memory is managed statically by the compiler.
+//Memory is allocated in the data segment of the memory. 
+//After the program ends, their memory is freed.
+
+int a, b = 5; 
 int main()
 {
-    //Declare string variable to hold user's name.
+    bool my_flag;
+    a = 7; //In general, for good coding practice, try not to use global variables.
+    my_flag = 0;
 
+    //Declare string variable to hold user's name.
+    std::string str;
     //Print the prompt asking user's name:
     std::cout << "What is your name?\n";
-    std::string str;
     std::cin >> str;
     std::cout << "Very nice to meet you, " << str << ".\n";
+    std::cout << "a =  " << a << std::endl;
+    std::cout << "b = " << b << std::endl;
+    //This below line will print out zero, as false is zero in C++.
+    std::cout << "flag = " << my_flag << std::endl;
+
+    unsigned int positive;
+    //Try to assign negative to unsigned variable.
+    //It will be wrapped around. Instead of -2, it will be 4294967294
+    positive = b - a;
+    std::cout << "b - a (unsigned) = " << positive <<std::endl;
+
+    std::cout <<std::endl;
     return (0);
 }
