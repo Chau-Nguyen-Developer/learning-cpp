4c964a014288d6df671a192a6f325e13f529d0d6
Chau Nguyen Developer (Christina) <chau.nguyen.dev@gmail.com>
2025-12-24 00:46:06 +0000

    Access unintialized array will give us dummy value
    Play around with arrays

--- src/Ch02/02_08b/CodeDemo.cpp
+++ src/Ch02/02_08b/CodeDemo.cpp
@@ -3,9 +3,21 @@
 // Arrays, by Eduardo Corpeño 
 
 #include <iostream>
+//#define AGE_LENGTH 4 //this is marco, and macros have no scope.
+
+
 
 int main(){
-    
+    const size_t AGE_LENGTH = 4;
+    int age[AGE_LENGTH];
+
+    std::cout << "The age array has " << AGE_LENGTH << "elements" << std::endl;
+    std::cout << "At index 0" << std::endl;
+    std::cout << age[0]<< std::endl;
+    std::cout << "At index 1" << std::endl;
+    std::cout << age[1] << std::endl;
+    std::cout << "Both values above are garbage number, as we did not initalize the array" << std::endl;
+
     std::cout << std::endl << std::endl;
     return (0);
 }
