18148f8c500ec66343f0307058a725dc97c15f4e
Chau Nguyen Developer (Christina) <chau.nguyen.dev@gmail.com>
2026-01-04 04:18:51 +0000

    Reviewed and practiced type casting

--- src/Ch02/02_09b/CodeDemo.cpp
+++ src/Ch02/02_09b/CodeDemo.cpp
@@ -3,9 +3,19 @@
 // Strings, by Eduardo Corpeño 
 
 #include <iostream>
+#include <string>
+#include <cstring>
 
 int main(){
+    const size_t LENGTH1 = 25;
+
+    char array_str1[LENGTH1] = "Hey guys! ";
+    char array_str2[] = "What's up?";
+
+    std::string std_str1 = "Hi everybody! ";
+    std::string std_str2 = "How's it going?";
     
+    std::cout << sizeof(array_str1) << std::endl;
     std::cout << std::endl << std::endl;
     return (0);
 }
--- src/Ch02/02_10b/CodeDemo.cpp
+++ src/Ch02/02_10b/CodeDemo.cpp
@@ -5,8 +5,23 @@
 #include <iostream>
 #include <cstdint>
 
-int main(){
+int main()
+{
+    float flt;
+    int32_t sgn;
+    int sgn2;
+    uint32_t unsgn;
+
+    flt = -7.66;
+    sgn = flt;
+    unsgn = sgn;
+
     
-    std::cout << std::endl << std::endl;
+    std::cout << "float: " << flt << std::endl;
+    std::cout << " int32: " << sgn  << std::endl;
+    std::cout << "uint32: " << (int32_t) unsgn << std::endl;
+    std::cout << std::endl;
+
+    // std::cout << std::endl << std::endl;
     return (0);
 }
