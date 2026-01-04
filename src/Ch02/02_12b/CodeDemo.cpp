577b5d0615b40089c94a348b2ef432aba699c589
Chau Nguyen Developer (Christina) <chau.nguyen.dev@gmail.com>
2026-01-04 04:57:15 +0000

    Reviewed and practiced enumeration class c++ style

--- src/Ch02/02_12b/CodeDemo.cpp
+++ src/Ch02/02_12b/CodeDemo.cpp
@@ -5,15 +5,25 @@
 #include <iostream>
 #include <cstdint>
 
-enum cow_purpose {dairy, meat, hide, pet};
+// enum cow_purpose {dairy, meat, hide, pet};
+//C-style enumeration
+// enum cow_purpose {dairy, meat, hide, pet};
+// enum grocery_section{canned, frozen, meat, laundry, dairy,};
+
+enum class cow_purpose {dairy, meat, hide, pet};
+enum class grocery_section{canned, frozen, meat, laundry, dairy,};
 
 int main(){
+    int meat = 8;
     int a;
-
+    cow_purpose b;
+    b = cow_purpose::pet;
     a = meat;
+    grocery_section c = grocery_section::meat;
 
     std::cout << "a = " << a << std::endl;
-
+    std::cout << "b = " << (int)b << std::endl;
+    std::cout << "c = " << (int)c << std::endl;
     std::cout << std::endl << std::endl;
     return (0);
 }
